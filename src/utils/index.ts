4e10d241fe328eea1eee7851f1e05ae39bad9b0a
Rauljauregi <53576209+Rauljauregi@users.noreply.github.com>
2023-09-28 17:00:25 +0200

    Initial commit

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export { sluglify, unsluglify } from './sluglify'
+export { cn } from './cn'
+export { getCategories, getPosts } from './post'
+export { remarkReadingTime } from './readTime'
---
+++ src/utils/post.ts
@@ -0,0 +1,14 @@
+import { getCollection } from 'astro:content'
+
+export const getCategories = async () => {
+	const posts = await getCollection('blog')
+	const categories = new Set(posts.map((post) => post.data.category))
+	return Array.from(categories)
+}
+
+export const getPosts = async (max?: number) => {
+	return (await getCollection('blog'))
+		.filter((post) => !post.data.draft)
+		.sort((a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf())
+		.slice(0, max)
+}
