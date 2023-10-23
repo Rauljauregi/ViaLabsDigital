0e9d1d4154fb7804197aba70542b6f5ac272417e
davidochoadev <davidochoa.dev@gmail.com>
2023-10-24 00:15:55 +0200

    ✅ Feat: Implemented Firebase and get list

---
+++ src/firebase/client.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyCAb7faHrNZLKtJ1YDmCbdsTepDckVZtjk",
+   authDomain: "newsletter-vialabsdigital.firebaseapp.com",
+   projectId: "newsletter-vialabsdigital",
+   storageBucket: "newsletter-vialabsdigital.appspot.com",
+   messagingSenderId: "965323008845",
+   appId: "1:965323008845:web:9707ff0e845255c1e72504",
+   measurementId: "G-V3WC9QSH8J"
+};
+
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
