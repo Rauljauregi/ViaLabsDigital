e1c7a6853cb0cce7f7a0af409611f2959dc4acba
davidochoadev <davidochoa.dev@gmail.com>
2023-10-24 14:00:19 +0200

    🛠 Fix: Fixed blog

--- src/pages/api/auth/getCustomToken.ts
+++ src/pages/api/auth/getCustomToken.ts
@@ -2,9 +2,12 @@ import type { APIRoute } from 'astro'
 import { app } from '../../../firebase/server'
 import { getAuth } from 'firebase-admin/auth'
 import { getUserFromFirestore } from "../../../utils/getUserFromFirestore";
+import { getFirestore } from 'firebase-admin/firestore';
 
 export const GET: APIRoute = async ({ request }) => {
 	const auth = getAuth(app);
+   const db = getFirestore(app)
+	const usersRef = db.collection('users')
 
    const email = request.headers.get('Authorization');
 
@@ -12,6 +15,15 @@ export const GET: APIRoute = async ({ request }) => {
    let token = ''
    if (querySnapshot.empty) {
       /* TRY REGISTRATION */
+      try {
+			await usersRef.add({
+            email
+			})
+		} catch (error) {
+			return new Response('Something went wrong', {
+				status: 500
+			})
+		}
       console.log('No matching user on get')
    } else {
       const userDoc = querySnapshot.docs[0]
