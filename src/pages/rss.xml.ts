a5aa6d6c5c3ff9c7d0e740b7eebe98ca701139da
davidochoadev <davidochoa.dev@gmail.com>
2023-11-22 04:04:19 +0100

    ✅ Feat: Adjust rss.xml

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,13 +4,53 @@ import { siteConfig } from '@/site-config'
 
 export async function GET() {
 	const posts = await getCollection('blog')
+	const newsletter = await getCollection('newsletter');
+
+	const combinedItems = [
+		{
+			title: 'Inteligencia Artificial',
+			description: 'Artículos sobre inteligencia artificial.',
+			pubDate: new Date(2023, 11, 1),
+			link: 'https://mindfulml.vialabsdigital.com/inteligencia-artificial',
+		},
+		{
+			title: 'Newsletter',
+			description: 'Artículos de la Newsletter',
+			pubDate: new Date(2023, 11, 1),
+			link: 'https://mindfulml.vialabsdigital.com/newsletter',
+		},
+		{
+			title: 'Deep Learning - Newsletter',
+			description: 'Artículos Newsletter sobre Deep Learning.',
+			pubDate: new Date(2023, 11, 1),
+			link: 'https://mindfulml.vialabsdigital.com/newsletter/Deep-Learning',
+		},
+		{
+			title: 'Inteligencia Artificial - Newsletter',
+			description: 'Artículos Newsletter sobre Inteligencia Artificial.',
+			pubDate: new Date(2023, 11, 1),
+			link: 'https://mindfulml.vialabsdigital.com/newsletter/Deep-Learning',
+		},
+		{
+			title: 'Machine Learning - Newsletter',
+			description: 'Artículos Newsletter sobre Machine Learning.',
+			pubDate: new Date(2023, 11, 1),
+			link: 'https://mindfulml.vialabsdigital.com/newsletter/Machine-Learning',
+		},
+		...posts.map((post) => ({
+		  ...post.data,
+		  link: `/post/${post.slug}/`,
+		})),
+		...newsletter.map((item) => ({
+		  ...item.data,
+		  link: `/newsletter/post/${item.slug}/`,
+		})),
+	 ];
+	 
 	return rss({
 		title: siteConfig.title,
 		description: siteConfig.description,
 		site: import.meta.env.SITE,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/${post.slug}/`
-		}))
+		items: combinedItems
 	})
 }
