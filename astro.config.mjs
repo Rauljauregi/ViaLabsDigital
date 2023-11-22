71282735013db4890b0fcfc3e4bf3e9110c6c197
davidochoadev <davidochoa.dev@gmail.com>
2023-11-22 03:18:48 +0100

    ✅ Feat: Created script to get sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,26 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 import vercel from '@astrojs/vercel/serverless';
+import { fileURLToPath } from 'node:url';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+console.log("__filename is : ",__filename);
+const __dirname = path.dirname(__filename);
+const directoryPath = path.join(__dirname, 'src', 'content', 'blog');
+const NewsletterDirectoryPath = path.join(__dirname, 'src', 'content', 'newsletter');
+const files = fs.readdirSync(directoryPath);
+const newsletterFiles = fs.readdirSync(NewsletterDirectoryPath);
+const blogUrl = 'https://mindfulml.vialabsdigital.com/post';
+const newsletterUrl = 'https://mindfulml.vialabsdigital.com/newsletter/post'
+const blogUrls = files.map((file) => {
+  const fileName = file.split('.')[0];
+  return `${blogUrl}/${fileName}`;
+});
+const newsletterUrls = newsletterFiles.map((file) => {
+	const fileName = file.split('.')[0];
+	return `${newsletterUrl}/${fileName}`;
+ });
 
 
 // https://astro.build/config
@@ -28,7 +48,17 @@ export default defineConfig({
 			},
 			drafts: true
 		}),
-		sitemap(),
+		sitemap({
+			customPages: [
+				'https://mindfulml.vialabsdigital.com/inteligencia-artificial',
+				'https://mindfulml.vialabsdigital.com/newsletter',
+				'https://mindfulml.vialabsdigital.com/newsletter/Deep-Learning',
+				'https://mindfulml.vialabsdigital.com/newsletter/Inteligencia-Artificial',
+				'https://mindfulml.vialabsdigital.com/newsletter/Machine-Learning'
+			].concat(blogUrls,newsletterUrls),
+			priority: 0.5,
+			changefreq: 'weekly'
+		 }),
 		tailwind()
 	]
 })
