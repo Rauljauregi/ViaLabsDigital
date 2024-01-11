28aac01229b8ce6499c7f81a0d524a540acffb96
davidochoadev <davidochoa.dev@gmail.com>
2024-01-11 14:50:40 +0100

    ✅ Feat: Changing Index

--- src/utils/newsletter.ts
+++ src/utils/newsletter.ts
@@ -9,6 +9,7 @@ export const getNewsletterCategories = async() => {
 export const getNewsletterPosts = async (max?: number) => {
 	return (await getCollection('newsletter'))
 		.filter((post) => !post.data.draft)
-		.sort((a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf())
-		.slice(0, max)
+		.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()) // Nota la modifica qui
+		.slice(0, max);
 }
+
