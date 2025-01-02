001e974883c3139d30f99b8c760d117d09ee3733
J.David Ochoa Arroba <davidochoa.dev@gmail.com>
2025-01-02 21:05:02 +0100

    🛠 Fix: Removing tina

--- tina/__generated__ 2/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '674518eb198db4ef90caebb299e51001662c52df', queries,  });
-export default client;
-  
