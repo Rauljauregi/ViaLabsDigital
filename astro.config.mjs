bd0ef57ba1d989461dea788d37ebeff6a4a543ee
J.David Ochoa Arroba <davidochoa.dev@gmail.com>
2025-01-02 21:58:15 +0100

    ✅ Feat: Install of sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,31 +3,31 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
-import vercel from '@astrojs/vercel/serverless';
-import { fileURLToPath } from 'node:url';
-import * as path from 'path';
+import vercel from '@astrojs/vercel/serverless'
+import { fileURLToPath } from 'node:url'
+import * as path from 'path'
+import fs from 'node:fs'
 import partytown from '@astrojs/partytown'
 
-const __filename = fileURLToPath(import.meta.url);
-console.log("__filename is : ",__filename);
-const __dirname = path.dirname(__filename);
-const directoryPath = path.join(__dirname, 'src', 'content', 'blog');
-const NewsletterDirectoryPath = path.join(__dirname, 'src', 'content', 'newsletter');
-const files = fs.readdirSync(directoryPath);
-const newsletterFiles = fs.readdirSync(NewsletterDirectoryPath);
-const blogUrl = 'https://mindfulml.vialabsdigital.com/post';
-const newsletterUrl = 'https://mindfulml.vialabsdigital.com/newsletter/post';
+const __filename = fileURLToPath(import.meta.url)
+console.log('__filename is : ', __filename)
+const __dirname = path.dirname(__filename)
+const directoryPath = path.join(__dirname, 'src', 'content', 'blog')
+const NewsletterDirectoryPath = path.join(__dirname, 'src', 'content', 'newsletter')
+const files = fs.readdirSync(directoryPath)
+const newsletterFiles = fs.readdirSync(NewsletterDirectoryPath)
+const blogUrl = 'https://mindfulml.vialabsdigital.com/post'
+const newsletterUrl = 'https://mindfulml.vialabsdigital.com/newsletter/post'
 
 const blogUrls = files.map((file) => {
-  const fileName = file.split('.')[0];
-  return `${blogUrl}/${fileName}`;
-});
+	const fileName = file.split('.')[0]
+	return `${blogUrl}/${fileName}`
+})
 
 const newsletterUrls = newsletterFiles.map((file) => {
-	const newsletterfileName = file.split('.')[0];
-	return `${newsletterUrl}/${newsletterfileName}`;
- });
-
+	const newsletterfileName = file.split('.')[0]
+	return `${newsletterUrl}/${newsletterfileName}`
+})
 
 // https://astro.build/config
 export default defineConfig({
@@ -58,14 +58,14 @@ export default defineConfig({
 				'https://mindfulml.vialabsdigital.com/newsletter/Deep-Learning',
 				'https://mindfulml.vialabsdigital.com/newsletter/Inteligencia-Artificial',
 				'https://mindfulml.vialabsdigital.com/newsletter/Machine-Learning'
-			].concat(blogUrls,newsletterUrls),
+			].concat(blogUrls, newsletterUrls),
 			priority: 0.5,
 			changefreq: 'weekly'
-		 }),
+		}),
 		partytown({
 			config: {
-				forward: ["dataLayer.push"],
-			},
+				forward: ['dataLayer.push']
+			}
 		}),
 		tailwind()
 	]
