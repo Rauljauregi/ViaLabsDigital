913d583cacd5fe426de1085686e4bbfa231fd73b
Raúl Jáuregui <rauljauregi@gmail.com>
2025-01-10 16:24:37 +0100

    10-enero-2025-g

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,62 +2,50 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import { remarkReadingTime } from './src/utils/readTime.ts';
-import vercel from '@astrojs/vercel';
-import { fileURLToPath } from 'node:url';
-import * as path from 'path';
 import partytown from '@astrojs/partytown';
 import rehypeKatex from 'rehype-katex';
+import remarkReadingTime from 'remark-reading-time'; // Reemplazo con paquete externo
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel(),
-	site: 'https://mindfulml.vialabsdigital.com/', // URL base del sitio web
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		rehypePlugins: [rehypeKatex],
-		drafts: true,
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true,
-		},
-	},
-	integrations: [
-		mdx({
-			syntaxHighlight: 'shiki',
-			rehypePlugins: [['rehype-katex', { throwOnError: true }]],
-			shikiConfig: {
-				theme: 'material-theme-palenight',
-				wrap: true,
-			},
-			drafts: true,
-		}),
-		sitemap({
-			customPages: [
-				'https://mindfulml.vialabsdigital.com/inteligencia-artificial',
-				'https://mindfulml.vialabsdigital.com/newsletter',
-			],
-			priority: (url) => {
-				if (url === 'https://mindfulml.vialabsdigital.com/') return 1.0; // Prioridad alta para la página principal
-				if (url.includes('/post')) return 0.8; // Mayor prioridad para posts de blog
-				if (url.includes('/newsletter')) return 0.6; // Prioridad media para newsletters
-				return 0.5; // Prioridad predeterminada
-			},
-			changefreq: (url) => {
-				if (url.includes('/post')) return 'weekly'; // Los posts se actualizan semanalmente
-				if (url.includes('/newsletter')) return 'monthly'; // Las newsletters se actualizan menos frecuentemente
-				return 'yearly'; // Páginas estáticas cambian muy raramente
-			},
-		}),
-		partytown({
-			config: {
-				forward: ['dataLayer.push'],
-			},
-		}),
-		tailwind(),
-	],
+  output: 'server',
+  adapter: vercel(),
+  site: 'https://mindfulml.vialabsdigital.com/',
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    drafts: true,
+    shikiConfig: {
+      theme: 'material-theme-palenight',
+      wrap: true,
+    },
+  },
+  integrations: [
+    mdx({
+      syntaxHighlight: 'shiki',
+      rehypePlugins: [['rehype-katex', { throwOnError: true }]],
+      shikiConfig: {
+        theme: 'material-theme-palenight',
+        wrap: true,
+      },
+      drafts: true,
+    }),
+    sitemap({
+      priority: (url) => {
+        if (url === 'https://mindfulml.vialabsdigital.com/') return 1.0;
+        if (url.includes('/post')) return 0.8;
+        if (url.includes('/newsletter')) return 0.6;
+        return 0.5;
+      },
+      changefreq: (url) => {
+        if (url.includes('/post')) return 'weekly';
+        if (url.includes('/newsletter')) return 'monthly';
+        return 'yearly';
+      },
+    }),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+    tailwind(),
+  ],
 });
