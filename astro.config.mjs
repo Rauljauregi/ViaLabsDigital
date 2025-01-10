e6eef85c463bf97aa1cb38138e25b4df36d9b222
Raúl Jáuregui <rauljauregi@gmail.com>
2025-01-10 16:29:55 +0100

    10-enero-2025-i

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { vercel } from '@astrojs/vercel';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
 import rehypeKatex from 'rehype-katex';
