3ab30b4c383352f4887e733c58600a59f35e4a0d
rjaureg <rauljauregi@gmail.com>
2025-01-11 23:25:57 +0100

    sitemap arreglado

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,39 +1,25 @@
 import { getCollection } from 'astro:content';
 
-interface Site {
-  site: string;
-}
+export async function getStaticPaths() {
+  const posts = await getCollection('blog');
+  const pages = ['', 'about', 'contact'];
+  const site = import.meta.env.SITE;
 
-export async function get({ site }: Site) {
-    const posts = await getCollection('blog');
-
-    const pages = [
-        { url: `${site}/`, priority: 1.0, changefreq: 'yearly' },
-        ...posts.map((post) => ({
-            url: `${site}/post/${post.slug}`,
-            priority: 0.8,
-            changefreq: 'weekly',
-        })),
-    ];
-
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  return {
+    body: `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages
-    .map(
-        (page) => `
-  <url>
-    <loc>${page.url}</loc>
-    <priority>${page.priority.toFixed(1)}</priority>
-    <changefreq>${page.changefreq}</changefreq>
-  </url>`
-    )
-    .join('')}
-</urlset>`;
-
-    return {
-        body: sitemap,
-        headers: {
-            'Content-Type': 'application/xml',
-        },
-    };
+  ${pages.map((page) => `
+    <url>
+      <loc>${site}${page ? `/${page}` : ''}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+    </url>
+  `).join('')}
+  ${posts.map((post) => `
+    <url>
+      <loc>${site}/blog/${post.slug}</loc>
+      <lastmod>${post.data.updatedDate || post.data.publishDate}</lastmod>
+    </url>
+  `).join('')}
+</urlset>`,
+  };
 }
