845222850391399129f2cabcc9e914899de1d641
rjaureg <rauljauregi@gmail.com>
2025-01-11 22:56:44 +0100

    sitemap arreglado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
@@ -39,11 +38,6 @@ export default defineConfig({
                 wrap: true,
             },
             drafts: true,
-        }),
-        sitemap({
-            customPages: [
-                'https://mindfulml.vialabsdigital.com/post',
-            ],
         }),
         tailwind(),
     ],
