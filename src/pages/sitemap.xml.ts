506a3e856f726e23a39d391ad8ca54a746265ea3
rjaureg <rauljauregi@gmail.com>
2025-01-12 00:02:47 +0100

    sitemap arreglado

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -3,31 +3,28 @@ import { getCollection } from 'astro:content';
 export const prerender = true;
 
 export async function GET() {
+  // URL base del sitio
+  const site = 'https://mindfulml.vialabsdigital.com';
+
   // Obtiene los posts de la colección 'blog'
   const posts = await getCollection('blog');
-
-  // Define las páginas estáticas
-  const staticPages = ['', 'about', 'contact']; // Asegúrate de incluir aquí tus páginas estáticas
-  const site = import.meta.env.SITE;
 
   // Genera el contenido XML
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${staticPages
-    .map((page) => `
-    <url>
-      <loc>${site}${page ? `/${page}` : ''}</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-    </url>
-  `)
-    .join('')}
+  <url>
+    <loc>${site}/</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </url>
   ${posts
-    .map((post) => `
+    .map(
+      (post) => `
     <url>
       <loc>${site}/post/${post.slug}</loc>
       <lastmod>${post.data.updated || post.data.published || new Date().toISOString()}</lastmod>
     </url>
-  `)
+  `
+    )
     .join('')}
 </urlset>`;
 
