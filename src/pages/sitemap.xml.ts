9a4639db54b6b42aae75939429627faa38984f20
Raúl Jáuregui <rauljauregi@gmail.com>
2025-01-11 19:32:04 +0100

    Fix sitemap.xml dynamic endpoint

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,26 +1,23 @@
 import { getCollection } from 'astro:content';
 
 export async function get({ site }) {
-    // Obtener colecciones de posts y newsletters
     const posts = await getCollection('blog');
     const newsletters = await getCollection('newsletter');
 
-    // Construir las páginas para el sitemap
     const pages = [
-        { url: `${site}/`, priority: 1.0, changefreq: 'yearly' }, // Página principal
+        { url: `${site}/`, priority: 1.0, changefreq: 'yearly' },
         ...posts.map((post) => ({
-            url: `${site}/post/${post.slug}`, // Ruta para cada post
+            url: `${site}/post/${post.slug}`,
             priority: 0.8,
             changefreq: 'weekly',
         })),
         ...newsletters.map((newsletter) => ({
-            url: `${site}/newsletter/post/${newsletter.slug}`, // Ruta para cada newsletter
+            url: `${site}/newsletter/post/${newsletter.slug}`,
             priority: 0.6,
             changefreq: 'monthly',
         })),
     ];
 
-    // Generar el contenido del sitemap en formato XML
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages
@@ -36,9 +33,9 @@ ${pages
 </urlset>`;
 
     return {
-        body: sitemap, // Contenido del sitemap
+        body: sitemap,
         headers: {
-            'Content-Type': 'application/xml', // Encabezado para indicar XML
+            'Content-Type': 'application/xml',
         },
     };
 }
