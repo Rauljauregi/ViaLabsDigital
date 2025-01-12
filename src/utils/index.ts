a43a83c1f6ac1ac1f859e1e7f4c6ba29fa736da6
Raúl Jáuregui <rauljauregi@gmail.com>
2025-01-12 15:29:44 +0100

    tags7

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,14 @@
 export { sluglify, unsluglify } from './sluglify'
 export { cn } from './cn'
+export { getCategories, getPosts } from './post'
+export { getNewsletterCategories, getNewsletterPosts } from './newsletter'
+export { remarkReadingTime } from './readTime'
 export {
 	getAllCollection,
-	getCategories,
 	getCategoryName,
 	getCategoryTitleFromSlug,
-	getPosts,
 	getTags,
 	getPostByTag,
 	getIndexPageByCategory,
 	filterPostsByCategory
 } from './post'
-export { remarkReadingTime } from './readTime'
