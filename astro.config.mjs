7a09a29c76ae583002e97495796f7f1898b364d7
Raúl Jáuregui <rauljauregi@gmail.com>
2025-02-18 15:35:32 +0100

    18 de febrero de 2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'; // Importa el módulo de sitemap
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 import { remarkReadingTime } from './src/utils/readTime.ts';
 import vercel from '@astrojs/vercel';
-import partytown from '@astrojs/partytown'; // Añade esta línea
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
-    site: 'https://mindfulml.vialabsdigital.com/',
+    site: 'https://mindfulml.vialabsdigital.com/', // Define tu dominio correctamente
     output: 'server',
     adapter: vercel(),
     vite: {
@@ -68,11 +69,19 @@ export default defineConfig({
         }),
         tailwind(),
         partytown({
-            // Configuración de Partytown
             config: {
                 forward: ["dataLayer.push", "gtag"],
-                debug: false // Cambia a true si necesitas debuggear
+                debug: false
             }
+        }),
+        sitemap({ 
+            filter: (page) => !page.includes('/drafts/'), // Excluye borradores si es necesario
+            customPages: [
+                'https://mindfulml.vialabsdigital.com/custom-page'
+            ], // Añade URLs manualmente si no están en Astro
+            entryLimit: 5000, // Límite de URLs por sitemap (para grandes sitios)
+            changefreq: 'weekly', // Frecuencia de actualización sugerida
+            priority: 0.8, // Prioridad en los motores de búsqueda
         })
     ],
 });
