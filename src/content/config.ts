a1d977537be5ca51b6c1bb1a05d4154d999f3765
Raul Jauregi <rauljauregi@gmail.com>
2025-09-22 18:44:06 +0200

    7 señales de que tu web está perdiendo clientes sin que lo sepas

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,29 +1,28 @@
 import { defineCollection, z } from 'astro:content'
-import { CATEGORIES} from '@/data/categories'
-
-
+import { CATEGORIES } from '@/data/categories'
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string().max(80),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z
-				.string()
-				.or(z.date())
-				.transform((val) => new Date(val)),
-			heroImage: z
-				.string()
-				.refine((path) => path.startsWith('/images/'), {
-				  message: 'La imagen debe estar en la carpeta /public/images/ y comenzar con /images/',
-				}),
-			category: z.enum(CATEGORIES),
-			tags: z.array(z.string()),
-			draft: z.boolean().default(false)
-		})
-});
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().max(80),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z
+        .string()
+        .or(z.date())
+        .transform((val) => new Date(val)),
+      heroImage: z
+        .string()
+        .refine((path) => path.startsWith('/images/'), {
+          message:
+            'La imagen debe estar en la carpeta /public/images/ y comenzar con /images/',
+        }),
+      category: z.enum(CATEGORIES),
+      tags: z.array(z.string()),
+      draft: z.boolean().default(false),
+      serviceUrl: z.string().url().optional(), // 👈 añadido
+    }),
+})
 
-
-export const collections = { blog}
+export const collections = { blog }
