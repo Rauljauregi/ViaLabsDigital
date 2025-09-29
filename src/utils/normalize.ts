4df7d8c2c4cb842b685363b23d803d69e271e711
rjaureg <rauljauregi@gmail.com>
2025-09-29 12:35:42 +0200

    acentos en URLs 2

--- src/utils/normalize.ts
+++ src/utils/normalize.ts
@@ -1,8 +1,2 @@
 export const normalize = (s: string) =>
-  s
-    .normalize('NFD')                      // separa acentos
-    .replace(/[\u0300-\u036f]/g, '')       // elimina tildes
-    .replace(/ñ/g, 'n')                    // reemplaza ñ → n
-    .toLowerCase()
-    .trim()
-    .replace(/\s+/g, '-');                 // espacios → guiones
+  s.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
